4857d8fbc3cdd627cd3881dc33056fc63596af08
Russel-T <russel.abraham123@gmail.com>
2024-07-16 15:27:19 -0500

    first commit

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+	public class Category
+	{
+        // Primary key attribute for Category Model: Id property
+        [Key]
+		public int Id { get; set; }
+
+        // Required attribute to ensure the Name property is not null or empty
+        [Required]
+		public string Name { get; set; }
+
+		public int DisplayOrder { get; set; }
+
+		// Default Constructor
+		public Category()
+		{
+		}
+	}
+}
+
